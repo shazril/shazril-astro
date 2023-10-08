497bc0513ab6e772a68f4c5792a9891fa7ce9f11
Shazril Shohaimi <shazril@live.jp>
2023-10-08 23:36:12 +0800

    rebrand to mine

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,13 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/",
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://shazril.com/",
+  author: "Shazril Shohaimi",
+  desc: "Laugh, live, learn",
+  title: "Shazril Shohaimi",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
-  postPerPage: 3,
+  postPerPage: 8,
 };
 
 export const LOCALE = ["en-EN"]; // set to [] to use the environment default
@@ -22,13 +22,13 @@ export const LOGO_IMAGE = {
 export const SOCIALS: SocialObjects = [
   {
     name: "Github",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com/shazril",
     linkTitle: ` ${SITE.title} on Github`,
     active: true,
   },
   {
     name: "Facebook",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://www.facebook.com/shazril/",
     linkTitle: `${SITE.title} on Facebook`,
     active: true,
   },
@@ -36,19 +36,19 @@ export const SOCIALS: SocialObjects = [
     name: "Instagram",
     href: "https://github.com/satnaing/astro-paper",
     linkTitle: `${SITE.title} on Instagram`,
-    active: true,
+    active: false,
   },
   {
     name: "LinkedIn",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://www.linkedin.com/in/shazrilshohaimi/",
     linkTitle: `${SITE.title} on LinkedIn`,
     active: true,
   },
   {
     name: "Mail",
-    href: "mailto:yourmail@gmail.com",
+    href: "mailto:shazril.shohaimi@gmail.com",
     linkTitle: `Send an email to ${SITE.title}`,
-    active: false,
+    active: true,
   },
   {
     name: "Twitter",
---
+++ src/content/blog/230817-what-if-there-was-no-devil.md
@@ -0,0 +1,23 @@
+---
+title: What if there was no devil for one day?
+author: Shaszril Shohaimi
+pubDatetime: 2023-08-17T05:06:31Z
+postSlug: what-if-there-was-no-devil
+featured: false
+draft: false
+tags:
+  - Life
+description: In the absence of the devil, Hafiz and me discuss the implications on sales and commerce.
+---
+
+A friend of mine, Hafiz from Sales, asked randomly, “Have you ever wondered if there is no devil, just for one day?”
+
+For sure, I thought, world will be at peace. No death due to people killing each other, people will pray like there is no tomorrow, and wealthy people will look after the poors.
+
+His answer was “There will be no sales, no commerce, no money flow”.
+
+“Devil” sparks human desire to acquire things. From this desire, people are willing to make dealings in order to gain the items. There will be profit taking for the transaction, thus, sales are made.
+
+This reminds me of a great sales book [Influence by Robert Cialdini](https://www.goodreads.com/book/show/28815.Influence). I highly recommend this book for any aspiring salesman, honestly. The book teaches few tricks that is so simple, yet every great salesman do them. The core message is that human buys when their emotion was triggered.
+
+I’m still optimistic, though, that if there were no devil for a day, all my pending invoices will be cleared within that day.
