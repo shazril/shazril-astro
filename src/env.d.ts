a6c3f58f48c2bc422b105431fac64afecbbba8e4
Shazril Shohaimi <shazril@live.jp>
2023-12-20 23:38:34 +0800

    add umami tracking

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly UMAMI_URL: string;
+  readonly UMAMI_CODE: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
